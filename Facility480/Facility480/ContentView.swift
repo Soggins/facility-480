470e2c45ea1fbf3d378d8c80aad17cbb7f2716dc
Hector Martin <hector.martin@cuatroochenta.com>
2022-04-11 11:24:27 +0200

    first commit

---
+++ Facility480/Facility480/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Facility480
+//
+//  Created by Hector Martin on 11/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Facility480/Facility480/Facility480App.swift
@@ -0,0 +1,17 @@
+//
+//  Facility480App.swift
+//  Facility480
+//
+//  Created by Hector Martin on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Facility480App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
